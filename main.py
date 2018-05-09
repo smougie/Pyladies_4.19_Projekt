bb593886054424d76b276428d710aab538a98700
smougie <smougie@gmail.com>
2018-05-09 19:15:29 +0200

    New function in main.py - readfile

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# Hello there :)
+def read_file():
+    filename = 'szwagropol_data/transactions.txt'
+
+    with open(filename, encoding='utf-8') as f_obj:
+        rows = []
+        for line in f_obj:
+            current_row = line.strip().split(';')
+            rows.append(current_row)
+
+        for i in range(5):
+            print(rows[i])
+read_file()
