83a8f9da0ebbfa7dafd00621906931ecd87deb76
smougie <smougie@gmail.com>
2018-05-09 21:40:17 +0200

    End of daily session - added few comments

--- data_loading.py
+++ data_loading.py
@@ -15,21 +15,23 @@ def open_filename(filename):
         for line in f_obj:
             current_row = line.strip().split(';')  # Usuwamy białe znaki oraz ';'
             current_row[0] = parse_date_from_string(current_row[0])  # Na indeksie 0 wykonujemy parsowanie daty
-            try:
+            try:  # Umieszamy w try except warunek, jeżeli ostatni indeks wywoła Value Error podczas parsowania to
+                  # pomijamy daną linię, w innym wypadku wykonujemy parsowanie stringa na float
                 current_row[-1] = float(current_row[-1])  # Zmieniamy string na float
             except ValueError:
                 continue
-            current_row[-2] = int(current_row[-2])
-            rows.append(current_row)
+            current_row[-2] = int(current_row[-2])  # Zamieniamy przeostatni indeks który jest str na int
+            rows.append(current_row)  # Dodajemy edytowaną linię do nowo utworzonej listy rows
 
-        return rows, columns_names
+        return rows, columns_names  # funkcja zwraca nazwę kolumn oraz ich wartość w dwóch różnych zmiennych
 
 # Funckja odpowiedzialna za liczenie sumy transakcji
 def transaction_value(rows):
-    QUANTITY_INDEX = -2
-    VALUE_INDEX = -1
-    transaction_value = 0
-    all_transaction_value_list = []
+    QUANTITY_INDEX = -2  # Indeks odpowiadający ilości produktów
+    VALUE_INDEX = -1  # Indeks odpowiadający wartości
+    transaction_value = 0  # Wartość całkowita transakcji
+    all_transaction_value_list = []  # Lista w której będziemy przechowywać sumę ilości produktu * jego wartość
+    # Pętla która dodaje do transaction_value oraz transaction list, przemnożoną ilość produtków razy jego wartość
     for lane in rows:
         transaction_value += lane[QUANTITY_INDEX] * lane[VALUE_INDEX]
         all_transaction_value_list.append(lane[QUANTITY_INDEX] * lane[VALUE_INDEX])
--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import datetime
 # Ścieżka pliku z danymi, który będziemy otwierać
 filename = 'szwagropol_data/transactions.txt'
 
-#Zapisujemy do zmiennej rows oraz column_names dane z modułu open_filename
+#Zapisujemy do zmiennej rows oraz column_names sformatowane dane z modułu open_filename
 rows, column_names = open_filename(filename)
 
 # Zapisujemy do zmiennych wartości z funckji transaction_value tj.
+# total_revenue - ogólny przychód, all_transaction_value_list - lista wartości każdego zakupu
 total_revenue, all_transaction_value_list = transaction_value(rows)
 
 
-# Tu jestem!!!
+# Tu jestem!!! wyszukiwanie danych po dacie.
 second_april = create_date(2018, 4, 2)
 
 rows_filtered = []
